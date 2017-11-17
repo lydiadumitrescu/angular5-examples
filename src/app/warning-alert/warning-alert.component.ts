e1898da9a1fa9a9d4e756ad50733df1a57e67b4a
CODE40\lydia.dumitrescu <lydia.dumitrescu@maxcode.net>
2017-11-17 11:40:31 +0200

    from tutorial

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-warning-alert',
+    templateUrl: './warning-alert.component.html',
+    styles: [`
+        p {
+            color: aquamarine
+        }
+    `]
+})
+export class WarningAlertComponent {
+
+}
